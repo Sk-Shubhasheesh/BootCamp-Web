fab138316eeebb9bf5ed2cfa1b2964e0b85e2b70
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2026-04-14 10:00:17 +0530

    trello backend db look like and backend initialization

---
+++ Week 10- Trello App backend/index.js
@@ -0,0 +1,52 @@
+// db looklike its dummy data
+
+/*
+const users = [{
+    id:1,
+    userName:"Shubhasheesh",
+    password: "13123"
+}, {
+    id:2,
+    userName:"raman",
+    password: "13123"
+}];
+
+const organizations = [{
+    id: 1,
+    title: "100xdevs",
+    description: "Learning coding platform",
+    admin:1,
+    members: [2]
+}, {
+    id: 2,
+    title: "ramans org",
+    description: "Experimenting",
+    admin:1,
+    members: []
+}];
+
+const boards = [{
+    id:1,
+    title: "100x school website frontend",
+    organizationId: 1
+}];
+
+const issues = [{
+    id:1,
+    title: "Add dark mode",
+    boardId: 1
+}, {
+    id:2,
+    title: "Allow admine to create more courses",
+    boardId: 1
+}]
+    */
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+
+
