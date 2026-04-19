44971b25ec01c1a2da57d92f2d5eee5e256cc41b
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2026-04-19 09:44:16 +0530

    Create Todo with Mongo & add some endpoint

---
+++ Week 11- Todo With Mongo/index.js
@@ -0,0 +1,29 @@
+// Initiaize simple Express App
+const express = require('express');
+const app = express();
+
+
+app.post("/signup", (req, res)=>{
+
+})
+
+app.post("/signin", (req, res)=>{
+
+})
+
+app.post("/todo", (req, res)=>{
+
+})
+
+app.get("/todo", (req, res)=>{
+
+})
+
+
+
+
+
+
+
+app.listen(3000);
+
---
+++ Week 11- Todo With Mongo/middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+function authMiddleware(req, res, next){
+    const token = req.header.token;
+    const decoded = jwt.verify(token, "123123")
+    if(decoded.userId){
+        req.userId = decoded.userId;
+        next()
+    } else{
+        res.status(403).json({
+            message: "Token Invalid or User Not found"
+        })
+    }
+
+}
+module.exports = {
+    authMiddleware:authMiddleware
+}
