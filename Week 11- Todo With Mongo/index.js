c050941c5a7612bd42d43c5eb85b77bd19ab8a35
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2026-04-19 10:37:45 +0530

    Schema done & Signup done

--- Week 11- Todo With Mongo/index.js
+++ Week 11- Todo With Mongo/index.js
@@ -1,9 +1,34 @@
 // Initiaize simple Express App
 const express = require('express');
+const { authMiddleware } = require('./middleware');
+const jwt = require('jsonwebtoken');
+
+const { todoModel,userModel } = require("./models");
+
 const app = express();
+app.use(express.json());
 
 
-app.post("/signup", (req, res)=>{
+app.post("/signup", async(req, res)=>{
+    const username =req.body.username;
+    const password = req.body.password;
+    const existingUser = await userModel.findOne({
+        username:username,
+        password: password
+    })
+    if(existingUser){
+        res.status(403).json({
+           message: "User with this username already exists" 
+        })
+        return
+    }
+    const newUser = await userModel.create({
+        username: username,
+        password:password
+    })
+    res.json({
+        id: newUser._id
+    })
 
 })
 
@@ -11,11 +36,13 @@ app.post("/signin", (req, res)=>{
 
 })
 
-app.post("/todo", (req, res)=>{
-
+app.post("/todo",authMiddleware, (req, res)=>{
+    const userId = req.userId;
+    
 })
 
-app.get("/todo", (req, res)=>{
+app.get("/todo",authMiddleware, (req, res)=>{
+    const userId = req.userId;
 
 })
 
