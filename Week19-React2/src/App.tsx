07b2b647f9466b752e8aa5a55549b1015a591aa4
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2026-06-01 21:03:06 +0530

    useState

--- Week19-React2/src/App.tsx
+++ Week19-React2/src/App.tsx
@@ -1,14 +1,16 @@
+import axios from "axios";
+import { useState } from "react";
 
 
-function App() {
-  return <div>
-    <b>hi  there</b>
-    <br /><br />
-    <LinkedinPost name={"Shubhasheesh"} content={"Hi, this is my first post "} />
-    <LinkedinPost name={"Shubham"} content={"Hi, there "}   />
-    <LinkedinPost backgrundColor={"red"}    name={"Shubham"} content={"Hi, there "} />
-  </div>
-}
+// function App() {
+//   return <div>
+//     <b>hi  there</b>
+//     <br /><br />
+//     <LinkedinPost name={"Shubhasheesh"} content={"Hi, this is my first post "} />
+//     <LinkedinPost name={"Shubham"} content={"Hi, there "}   />
+//     <LinkedinPost backgrundColor={"red"}    name={"Shubham"} content={"Hi, there "} />
+//   </div>
+// }
 
 // component
 // function => argument
@@ -22,13 +24,50 @@ content: "Hi.."
  
  */
 
-function LinkedinPost(props){
+// function LinkedinPost(props){
+//   return <div style={{margin:10,padding: 20, border:"1px solid black",borderRadius:5, backgroundColor: props.backgrundColor}}>
+//     <div>{props.name}</div>
+//     <div>{props.content}</div>
+//   </div>
+
+// }
+
+
+
+
+function App(){
+  let [data, setData] = useState([]);
+
+  axios.get("https://jsonplaceholder.typicode.com/todos")
+  .then(response => {
+    setData(response.data);
+  })
+
+  // convert array of objects in array of component
+// [{title: "go to gym"}, {title: "east"}]
+// [<Todo title ={"go to gym"}]
+
+  return <div>
+    {data.map(todo=> <Todo title={todo.title}  />)}
+  </div>
+
+  // Problem with useState - triggering infinite render loops
+
+}
+
+
+function Todo(props){
   return <div style={{margin:10,padding: 20, border:"1px solid black",borderRadius:5, backgroundColor: props.backgrundColor}}>
-    <div>{props.name}</div>
-    <div>{props.content}</div>
+    <div>{props.title}</div>
   </div>
 
 }
+
+
+
+
+
+
 
 
 export default App
